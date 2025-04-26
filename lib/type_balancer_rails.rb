8694dd66302c192ecc3286a18ecd3b34d81e6f52
Carl Smith <carl@llwebconsulting.com>
2025-04-26 16:59:42 -0400

    fix error loading Railtie

--- lib/type_balancer_rails.rb
+++ lib/type_balancer_rails.rb
@@ -8,4 +8,5 @@ require 'type_balancer'
 require_relative 'type_balancer/rails/version'
 require_relative 'type_balancer/rails'
 require 'type_balancer/rails/collection_methods'
-require 'type_balancer/rails/railtie'
+
+require 'type_balancer/rails/railtie' if defined?(Rails)
