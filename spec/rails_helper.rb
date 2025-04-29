59d24bd5af21d68175e5d63a91a63679baef4df5
Carl Smith <carl@llwebconsulting.com>
2025-04-29 19:36:46 -0400

    ignore files in example

---
+++ spec/rails_helper.rb
@@ -0,0 +1,18 @@
+require 'database_cleaner/active_record'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.fixture_path = "#{Rails.root}/test/fixtures"
+  config.use_transactional_fixtures = true
+  config.include ActiveRecord::TestFixtures
+end
